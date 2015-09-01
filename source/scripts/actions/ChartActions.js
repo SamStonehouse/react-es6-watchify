995fde73ef5871cc5533f343a837a882cfcada58
Sam Stonehouse <sstonehouse@scottlogic.com>
2015-09-01 17:02:03 +0100

    Updated chart to automatically-updating line chart

--- source/scripts/actions/ChartActions.js
+++ source/scripts/actions/ChartActions.js
@@ -8,15 +8,4 @@ class ChartActions {
 
 var chartActions = alt.createActions(ChartActions);
 
-setInterval(function() {
-
-	var val1 = Math.random() * 10;
-	var val2 = Math.random() * 10;
-
-	chartActions.updateChart([
-		{id: '5fbmzmtc', x: 7, y: 41, z: val1},
-		{id: 's4f8phwm', x: 11, y: 45, z: val2}
-	]);
-}, 1000);
-
 export default chartActions;
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -9,6 +9,19 @@
 import Chart from './components/chart';
 import React from 'react';
 
+import ChartStore from './stores/ChartStore';
+import ChartActions from './actions/ChartActions';
+
+setInterval(function() {
+	var chartData = ChartStore.getState().chartData;
+
+	chartData.shift();
+
+	chartData.push( { val: Math.random() * 200 });
+
+	ChartActions.updateChart(chartData);
+}, 20);
+
 var App = React.createClass({
 
 	render: function() {
