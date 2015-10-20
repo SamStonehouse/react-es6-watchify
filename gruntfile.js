94aaae755a032ca00eb3f2f62235dc4a0899ac15
Sam Stonehouse <sgstonehouse@googlemail.com>
2015-10-20 22:22:05 +0100

    Fixed missing reference to build directory

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		clean: {
-			build: 	["build/"],
+			build: 	[buildDir],
 		},
 		copy: {
 			assets: {
